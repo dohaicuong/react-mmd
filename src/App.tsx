e1516e573493b28f98d0ada398bf8f3a7913e92a
dohaicuong <beatyshot@gmail.com>
2020-04-24 21:16:14 +1000

    Model component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Canvas, useFrame, useThree } from 'react-three-fiber'
-import { useLoader } from 'react-three-fiber'
-import { MMDAnimationHelper } from 'three/examples/jsm/animation/MMDAnimationHelper'
-import { MMDLoader } from 'three/examples/jsm/loaders/MMDLoader'
-import { AudioListener, Audio, AudioLoader } from 'three'
+import { Canvas } from 'react-three-fiber'
+import Model from './components/Model'
 
 export default () => {
   return (
@@ -13,10 +10,10 @@ export default () => {
         camera={{ position: [10, 10, 40] }}
       >
         <ambientLight />
-        <pointLight position={[50, 50, 50]} intensity={0.5} distance={3} decay={2} />
+        <pointLight position={[10, 10, 10]} intensity={1} distance={1} decay={1} />
 
         <React.Suspense fallback={null}>
-          <Asset
+          <Model
             url='resources/models/baby_miku/baby_miku.pmx'
             motion='resources/motions/wavefile/motion.vmd'
             audio='resources/motions/wavefile/audio.mp3'
@@ -26,45 +23,4 @@ export default () => {
       </Canvas>
     </>
   )
-}
-
-const Asset = ({ url, motion, audio, camera }: any) => {
-  const three = useThree()
-  const mmd = useLoader(MMDLoader, url)
-  const loader = React.useMemo(() => new MMDLoader(), [])
-  const helper = React.useMemo(() => new MMDAnimationHelper(), [])
-  const audioLoader = React.useMemo(() => new AudioLoader(), [])
-
-  const [ready, setReady] = React.useState(false)
-  React.useEffect(() => {
-    if(motion && mmd.visible) {
-      // load animation and pour into mesh
-      loader.loadWithAnimation(url, motion, ({ animation }) => helper.add(mmd, { animation, physics: true }))
-      // load camera motion and pour into camera
-      loader.loadAnimation(camera, three.camera, cameraAnimation => {
-        // @ts-ignore
-        helper.add(three.camera, { animation: cameraAnimation })
-      })
-      // load audio
-      audioLoader.load(audio, buffer => {
-        const listener = new AudioListener()
-        const audio = new Audio(listener).setBuffer(buffer)
-        listener.position.z = 1
-        helper.add(audio, { delayTime: 160 * 1 / 30 })
-        setReady(true)
-      })
-    }
-
-
-  // eslint-disable-next-line
-  }, [motion, mmd.visible])
-
-  useFrame((_, delta) => {
-    if (ready) {
-      // console.log(three.camera)
-      return helper.update(delta)
-    }
-  })
-
-  return <primitive object={mmd} dispose={null} />
 }
--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,20 +1,54 @@
 import React from 'react'
-// @ts-ignore
-import usePromise from 'react-promise-suspense'
+import { useFrame, useThree } from 'react-three-fiber'
+import { useLoader } from 'react-three-fiber'
+import { MMDAnimationHelper } from 'three/examples/jsm/animation/MMDAnimationHelper'
+import { MMDLoader } from 'three/examples/jsm/loaders/MMDLoader'
+import { AudioListener, Audio, AudioLoader } from 'three'
 
 type ModelProps = {
   url: string
   motion?: string
-  pose?: string
+  audio?: string
+  camera?: string
 }
-type LoaderData = {
-  data: any
-  objects: any[]
-}
-const Model: React.FC<ModelProps> = ({ url, motion, pose }) => {
-  const results = usePromise<LoaderData>(() => {
+const Model: React.FC<ModelProps> = ({ url, motion, audio, camera }: any) => {
+  const three = useThree()
+  const mmd = useLoader(MMDLoader, url)
+  const loader = React.useMemo(() => new MMDLoader(), [])
+  const helper = React.useMemo(() => new MMDAnimationHelper(), [])
+  const audioLoader = React.useMemo(() => new AudioLoader(), [])
 
-  }, [])
-  return null
+  const [ready, setReady] = React.useState(false)
+  React.useEffect(() => {
+    if (motion && mmd.visible) {
+      // load animation and pour into mesh
+      loader.loadWithAnimation(url, motion, ({ animation }) => helper.add(mmd, { animation, physics: true }))
+      // load camera motion and pour into camera
+      loader.loadAnimation(camera, three.camera, cameraAnimation => {
+        // @ts-ignore
+        helper.add(three.camera, { animation: cameraAnimation })
+      })
+      // load audio
+      audioLoader.load(audio, buffer => {
+        const listener = new AudioListener()
+        const audio = new Audio(listener).setBuffer(buffer)
+        listener.position.z = 1
+        helper.add(audio, { delayTime: 160 * 1 / 30 })
+        setReady(true)
+      })
+    }
+
+
+    // eslint-disable-next-line
+  }, [motion, mmd.visible])
+
+  useFrame((_, delta) => {
+    if (ready) {
+      // console.log(three.camera)
+      return helper.update(delta)
+    }
+  })
+
+  return <primitive object={mmd} dispose={null} />
 }
 export default Model
